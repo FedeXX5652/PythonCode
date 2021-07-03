1b32e4aeee0fb42eb2f6121a604d1b9a77eea23d
FedeXX <fedegalant@gmail.com>
2021-07-03 16:11:37 -0300

    testing

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,43 @@
-l1=[1,2,3,4]
-l2=l1
-l1[0] = 1000
+preguntas = ["Las ballenas son mamiferos?", "La tierra gira al rededor del sol", "Los pulpos son mamiferos"]
+respuestas = ["Si", "Si", "No"]
 
-print(l1, l2)
+while True:      #Lo repite infinitas veces
+    accion = input("Que decea hacer?\n 1. Quiz\n 2. Añadir preguntas\n 3. Ver el quiz\n Ingrese el numero: ")
+    print("\n")
+
+    if accion == "1": #Accion 1
+        puntos = 0
+        for x in range(len(preguntas)): #tomo el tamaño de la lista, osea la cantidad de preguntas y respuestas totales
+            print(preguntas[x])
+            respuesta_ingresada = input()
+
+            if respuesta_ingresada.lower() == respuestas[x].lower(): #si la respuresta ingresada coincide con la respuesta de la preunta X en la lista
+                print("Correcto!"+ "\n")
+                puntos += 1
+            elif respuesta_ingresada.lower() != respuestas[x].lower(): #si la respuresta ingresada NO coincide con la respuesta de la preunta X en la lista
+                print("Incorrecto, la respuesta era: "+respuestas[x].lower())
+                print("Respuesta ingresada: "+respuesta_ingresada+ "\n")
+            x+=1
+        
+        print("Puntaje total: "+str(puntos))
+    
+    elif accion == "2": #Accion 2
+        #Recibo la nueva pregunta con su respuesta
+        pregunta_nueva = input("Ingrese la nueva pregunta: ")
+        respuesta_nueva = input("Ingrese la respuesta: ")
+
+        #Las añado a sus respectivas listas
+        preguntas.append(pregunta_nueva)
+        respuestas.append(respuesta_nueva)
+
+    elif accion == "3":#Accion 3
+        for x in range(len(preguntas)): #tomo el tamaño de la lista, osea la cantidad de preguntas y respuestas totales
+            print("Pregunta "+x+": "+preguntas[x]) #muestro las listas
+            print("Respuresta de "+x+": "+respuestas[x]+"\n")
+    
+    else: #Acciones restantes
+        print("Ingrese una acción valida")
+
+    print("-------------------------------------------------------------------")
+
+
