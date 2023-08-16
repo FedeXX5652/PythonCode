312dea76faa7a693c54b98aafdad9982029632a1
Federico Galante <54709342+FedeXX5652@users.noreply.github.com>
2023-08-16 18:57:30 -0300

    configurator

--- MagicCircle/draw.py
+++ MagicCircle/draw.py
@@ -1,4 +1,5 @@
-import scipy
+import operator
+import random
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -6,7 +7,7 @@ plt.rcParams["figure.figsize"] = [7, 7]
 plt.rcParams["figure.autolayout"] = True
 
 G = nx.Graph()
-n = 12
+n = 11
 G.add_nodes_from([x+1 for x in range(n)])
 
 def add_edges(G: nx.Graph, conn: int):
@@ -30,41 +31,92 @@ def edge_color_func(n1, n2):
     colors = ["black", "blue", "green", "red", "yellow", "orange", "purple", "pink"]
     return colors[distance-1]
 
+def row_factory(num: int):
+    row = [0 for _ in range(n-1)]
+    num_bin = bin(num)[2:]
+    num_list = [int(x) for x in num_bin]
+    
+    if len(num_list) > n - 1:
+        raise ValueError(f"Input number {num} produces a binary representation longer than n - 1 digits.")
+    
+    for i in range(len(num_list)):
+        row[-(i + 1)] = num_list[-(i + 1)]
+    
+    row = (row+[1])
+    print(row, len(row))
+    return row
 
 def set_level(level: int):
-    ns = [1 if x == level else 0 for x in range(n-1)]
-    ns.reverse()
-    print(ns)
-    return ns
+    # level goes from 0 to 9
+    # must add 1 to level to get correct index and set to binary and set the end to 1
+    print("set level: ")
+    return row_factory(level)
 
-def set_school(school: str, mat: list):
-    pass
 
-def set_dmg_type(dmg_type: str, mat: list):
-    pass
+schools = ["abjuration", "conjuration", "divination", "enchantment", "evocation", "illusion", "necromancy", "transmutation"]
+def set_school(school: str):
+    index = schools.index(school)
+    print("set school:")
+    return row_factory(index)
 
-def set_area_type(area_type: str, mat: list):
-    pass
 
-def set_range(range_: str, mat: list):
-    pass
-    
-def props_to_mat(level:int, school:str, dmg_type: str, area_type: str, range_: str):
-    mat = [[0 for x in range(n-1)] for y in range(n-1)]
-    mat[0] = set_level(level)
-    # mat[1] = set_school(school, mat[1])
-    # mat[2] = set_dmg_type(dmg_type, mat[2])
-    # mat[3] = set_area_type(area_type, mat[3])
-    # mat[4] = set_range(range_, mat[4])
-    print(mat)
+dmg_types = ["acid", "bludgeoning", "cold", "damage", "extra", "fire", "force", "lightning", "necrotic", "nonmagical", "piercing", "poison", "psychic", "radiant", "slashing", "thunder"]
+def set_dmg_type(dmg_type: str):
+    index = dmg_types.index(dmg_type)
+    print("set dmg type:")
+    return row_factory(index)
+
+def pad(num: str, ns: int = n):
+    pad = "0" * (ns - len(num))
+    return pad + num
+
+area_types = {"circle":pad("1111"), "cone/sphere": pad("1011"), "cone":pad("1001"),
+                  "cube":pad("1"), "cylinder":pad("100011"), "line":pad("11"), "multiple targets/sphere":pad("111"),
+                  "multiple targets":pad("101"), "none":pad("100101"), "single target/cone": pad("11011"),
+                  "single target/cube":pad("10111"), "single target/multiple targets":pad("11001"), "single target/sphere":pad("11101"),
+                  "single target/wall":pad("11111"), "single target":pad("10101"), "sphere/cylinder":pad("10011"),
+                  "sphere":pad("10001"), "square":pad("1101"), "wall":pad("100001")}
+def set_area_type(area_type: str):
+    print("set area type: ", [int(x) for x in area_types[area_type]])
+    return [int(x) for x in area_types[area_type]]
+
+ranges = ["10ft radius", "100ft line", "15ft cone", "15ft cube",
+            "15ft radius", "30ft cone", "30ft line", "30ft radius",
+            "5ft radius", "60ft cone", "60ft line", "1mi point",
+            "10ft point", "1000ft point", "120ft point", "150ft point",
+            "30ft point", "300ft point", "5ft point", "500ft point",
+            "60ft point", "90ft point", "self", "sight",
+            "special", "touch"]
+def set_range(range_: str):
+    index = ranges.index(range_)
+    print("set range:")
+    return row_factory(index)
+
+def props_to_mat(level:int=-1, school:str="", dmg_type: str="", area_type: str="", range_: str=""):
+    mat = [[] for y in range(5)]
+    mat[0] = set_level(level) if level != -1 else [0 for x in range(n+1)]
+    mat[1] = set_school(school) if school != "" else [0 for x in range(n+1)]
+    mat[2] = set_dmg_type(dmg_type) if dmg_type != "" else [0 for x in range(n+1)]
+    mat[3] = set_area_type(area_type) if area_type != "" else [0 for x in range(n+1)]
+    mat[4] = set_range(range_) if range_ != "" else [0 for x in range(n+1)]
     return mat
 
+level = random.randint(0,9)
+school = random.choice(schools)
+dmg_type = random.choice(dmg_types)
+area_type = random.choice(list(area_types.keys()))
+range_ = random.choice(ranges)
 
-graph(G,props_to_mat(level=1, school="evocation", dmg_type="cold", area_type="circle", range_="100ft"))
+graph(G, props_to_mat(level=level, school=school, dmg_type=dmg_type, area_type=area_type, range_=range_))
 
 plt.figure()
 edge_color = [edge_color_func(n1, n2) for n1, n2 in G.edges()]
-pos = nx.kamada_kawai_layout(G)
+pos = nx.circular_layout(G)
 pos = {n: pos[n] for n in sorted(G.nodes(), key=lambda n: n)}
-nx.draw(G, node_color="black", edge_color=edge_color, width=1, with_labels=False, font_color="white", pos=pos, connectionstyle="arc3,rad=-0.4", arrows=True)
+nx.draw(G, node_color="black", edge_color=edge_color, width=1, with_labels=True, font_color="white", pos=pos, arrows=True, connectionstyle="arc3,rad=-0.15")
+plt.text(0.5, -0.11, f"LEVEL: {level}", ha="center", fontsize=12, bbox=dict(facecolor='white', edgecolor='black', alpha=0.7))
+plt.text(0.5, -0.22, f"SCHOOL: {school}", ha="center", fontsize=12, bbox=dict(facecolor='white', edgecolor='blue', alpha=0.7))
+plt.text(0.5, -0.33, f"DAMAGE TYPE: {dmg_type}", ha="center", fontsize=12, bbox=dict(facecolor='white', edgecolor='green', alpha=0.7))
+plt.text(0.5, -0.44, f"AREA TYPE: {area_type}", ha="center", fontsize=12, bbox=dict(facecolor='white', edgecolor='red', alpha=0.7))
+plt.text(0.5, -0.55, f"RANGE: {range_}", ha="center", fontsize=12, bbox=dict(facecolor='white', edgecolor='yellow', alpha=0.7))
 plt.show()
